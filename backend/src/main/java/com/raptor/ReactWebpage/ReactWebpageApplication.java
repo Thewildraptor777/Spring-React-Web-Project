ca5e8c0ecce4f124e897b08174e87985e82daec5
Tyler Herron <tylerherron7777@gmail.com>
2023-06-30 20:40:04 -0500

    add a spring backend to project

---
+++ backend/src/main/java/com/raptor/ReactWebpage/ReactWebpageApplication.java
@@ -0,0 +1,13 @@
+package com.raptor.ReactWebpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactWebpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactWebpageApplication.class, args);
+	}
+
+}
